04ca5b8b36797b51d141e76409f8161172a39bbd
Gustavo Coelho <gutorc@hotmail.com>
2016-06-11 14:26:12 -0300

    Help friend
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- src/cs_friendhelp/models.py
+++ src/cs_friendhelp/models.py
@@ -1,13 +1,15 @@
 from codeschool import models
-from cs_questions.models import CodingIoResponse
+from cs_questions.models import CodingIoResponse, Question
 from cs_auth.models import FriendshipStatus
 # Create your models here.
 
 class HelpSession(models.Model):
     response = models.ForeignKey(CodingIoResponse)
+    question = models.ForeignKey(Question)
     owner = models.ForeignKey(models.User,related_name="help_sessions_as_owner")
     helper = models.ForeignKey(models.User,related_name="help_sessions_as_helper")
     comment = models.TextField()
+    created = models.DateField(auto_now_add=True) 
 
 class LineComment(models.Model):
     help_session = models.ForeignKey(HelpSession,related_name="comments_on_line")
--- src/cs_friendhelp/views.py
+++ src/cs_friendhelp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import HelpSession
 import srvice
 
 # Create your views here.
 @srvice.api
 def friendhelper(request, friend_pk, reponse_pk, question_pk):
-	...
-
+	hpSession = HelpSession()
+	hpSession.owner	= request.user 
+	hpSession.helper = friend_pk
+	hpSession.question = question_pk
+	hpSession.response = response_pk
+	hpSession.save()
 	return None
 
 
